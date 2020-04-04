f542ff6602addf315c43cfe1e6350a42e54c751a
marlon <marlonmiguel1993@gmail.com>
2020-04-04 02:46:36 -0300

    Adicionado produtos e associação com categorias

--- src/main/java/com/marlon/curso/CursoApplication.java
+++ src/main/java/com/marlon/curso/CursoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marlon.curso.domain.Categoria;
+import com.marlon.curso.domain.Produto;
 import com.marlon.curso.repositories.CategoriaRepository;
+import com.marlon.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository repo;
+	@Autowired
+	private ProdutoRepository repo1;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
@@ -25,7 +29,19 @@ public class CursoApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria("Informatica");
 		Categoria cat2 = new Categoria("Depósito");
 		
+		Produto pro1 = new Produto(null, "Computador", 2000.00);
+		Produto pro2 = new Produto(null, "Impressora", 300.00);
+		Produto pro3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(pro1,pro2,pro3));
+		cat2.getProdutos().addAll(Arrays.asList(pro2));
+		
+		pro1.getCategorias().addAll(Arrays.asList(cat1));
+		pro2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		pro3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		repo.saveAll(Arrays.asList(cat1, cat2));
+		repo1.saveAll(Arrays.asList(pro1, pro2, pro3));
 	}
 
 }
